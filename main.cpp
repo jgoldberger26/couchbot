19ca61baac7a1b1e22d11914cde43416c5e46d14
TagCiccone <tagciccone@gmail.com>
2025-04-20 15:53:20 -0400

    code to autodetect xbox controller

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 #include "gpio_bindings.h"
 
+#include <fstream>
+
 void setRevLeft(bool r) {
     std::cout<< "FIRING " << r << std::endl;
     setReverse(r, LEFT);
@@ -14,7 +16,24 @@ int main() {
     InputHub ihub;
     OutputHub ohub;
 
-    ihub.makeReadThread("event2", XBOX);
+    std::ifstream f("/proc/bus/input/devices");
+
+    std::string line;
+    bool fi = false;
+    std::string event;
+    while (std::getline(f, line)) {
+        if (!fi && line.find("Xbox") != std::string::npos) {
+            fi = true;
+        } else if (fi && line.find("Handlers") != std::string::npos) {
+            auto r = line.find("event");
+            event = line.substr(r, 6);
+            break;
+        }
+    } 
+
+    f.close();
+
+    ihub.makeReadThread(event, XBOX);
 
     ohub.addAction(
         action(
