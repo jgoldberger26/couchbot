7c9e3795b42f86e8f27cbb3dafa4214505b1f4ab
TagCiccone <tagciccone@gmail.com>
2025-09-21 12:07:44 -0400

    add libgpiod to installscript

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ sudo apt install -y build-essential gcc-arm-linux-gnueabihf
 sudo apt install -y build-essential g++-arm-linux-gnueabihf
 
 # git clone https://github.com/joan2937/pigpio.git lib/pigpio
+git clone https://github.com/brgl/libgpiod.git lib/libgpiod
 
 # make CROSS_PREFIX=arm-linux-gnueabihf- -C lib/pigpio
 # sudo make CROSS_PREFIX=arm-linux-gnueabihf- install -C lib/pigpio
